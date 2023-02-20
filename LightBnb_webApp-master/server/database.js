5511ab09eba4863ed6f4af79118e1f094be10d44
thahira97 <thahirasheerin3@gmail.com>
2023-02-20 18:36:57 -0500

    Change in getUserWithId

--- LightBnb_webApp-master/server/database.js
+++ LightBnb_webApp-master/server/database.js
@@ -41,7 +41,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      } else {
+        console.log(result.rows[0]);
+        return result.rows[0];
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getUserWithId = getUserWithId;
 
